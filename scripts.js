960246ccab8cea5419b5565dd4ab416cb2b47aae
Mike Manger <mike.manger@lighthouseuk.net>
2022-01-04 16:01:14 +0000

    Tidy up scripts file.

--- scripts.js
+++ scripts.js
@@ -1,51 +1,57 @@
+/* globals $, VANTA */
+
 $(function() {
-    $.getJSON( './dictionary_m.json', function( data ) {
-        var word_1 = data[ Math.floor( Math.random() * data.length ) ];
-        var word_2 = data[ Math.floor( Math.random() * data.length ) ];
-        document.title = "Mike Manger's " + word_1 + " " + word_2;
-    });
+	$.getJSON( './dictionary_m.json', function( data ) {
+		const word_1 = data[ Math.floor( Math.random() * data.length ) ];
+		const word_2 = data[ Math.floor( Math.random() * data.length ) ];
+		document.title = "Mike Manger's " + word_1 + " " + word_2;
+	});
 });
 
-let background = 'white';
-let primaryColor = '#1d1f21';
+const DEFAULT_BACKGROUND = 'white';
+const DEFAULT_PRIMARY_COLOR = '#1d1f21';
+const DARK_BACKGROUND = '#1d1f21';
+const DARK_PRIMARY_COLOR = '#fa3b7d';
+let background = DEFAULT_BACKGROUND;
+let primaryColor = DEFAULT_PRIMARY_COLOR;
 
 if (window.matchMedia) {
-    const IS_DARK = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    if ( IS_DARK ) {
-        background = '#1d1f21';
-        primaryColor = '#fa3b7d';
-    }
-    // Check if dark mode changes
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function(e) {
-        updateDarkMode( e.matches );
-    });
+	const IS_DARK = window.matchMedia('(prefers-color-scheme: dark)').matches;
+	if ( IS_DARK ) {
+		background = '#1d1f21';
+		primaryColor = '#fa3b7d';
+	}
+	// Check if dark mode changes
+	window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function(e) {
+		updateDarkMode( e.matches );
+	});
 }
 
 const BACKGROUND = VANTA.NET({
-    el: 'body',
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    maxWidth: 400,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    backgroundColor: background,
-    color: primaryColor,
-    maxDistance: 17.00,
-    spacing: 20.00
+	el: 'body',
+	mouseControls: true,
+	touchControls: true,
+	gyroControls: false,
+	minHeight: 200.00,
+	minWidth: 200.00,
+	maxWidth: 400,
+	scale: 1.00,
+	scaleMobile: 1.00,
+	backgroundColor: background,
+	color: primaryColor,
+	maxDistance: 17.00,
+	spacing: 20.00
 });
 
 function updateDarkMode( isDark ) {
-    let background = 'white';
-    let primaryColor = '#1d1f21';
-    if ( isDark ) {
-        background = '#1d1f21';
-        primaryColor = '#fa3b7d';
-    }
-    BACKGROUND.setOptions({
-        backgroundColor: background,
-        color: primaryColor
-    })
+	let background = DEFAULT_BACKGROUND;
+	let primaryColor = DEFAULT_PRIMARY_COLOR;
+	if ( isDark ) {
+		background = DARK_BACKGROUND;
+		primaryColor = DARK_PRIMARY_COLOR;
+	}
+	BACKGROUND.setOptions({
+		backgroundColor: background,
+		color: primaryColor
+	});
 }
